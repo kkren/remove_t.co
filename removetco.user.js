b07aa0a2de1abdd2744b30ab00897acfe49ac3ed
kkren <tangjl55@gmail.com>
2017-08-22 21:15:21 +0800

    beautify code

--- removetco.user.js
+++ removetco.user.js
@@ -9,21 +9,22 @@
 // @version 0.1
 // ==/UserScript==
 (function() {
-    function async_load() {
-        var s = document.createElement("script");
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js";
-        var x = document.getElementsByTagName("script")[0];
-        x.parentNode.insertBefore(s, x);
+  function async_load() {
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js";
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(s, x);
+  }
+  if (window.attachEvent) window.attachEvent("onload", async_load);
+  else window.addEventListener("load", async_load, false);
+  var i = 0;
+  window.onscroll = function() {
+    var urls = $("[data-expanded-url]").length;
+    for (; i < urls; i++) {
+      var expanded = $("[data-expanded-url]").eq(i).attr("data-expanded-url");
+      $("[data-expanded-url]").eq(i).attr("href", expanded);
     }
-    if (window.attachEvent) window.attachEvent("onload", async_load); else window.addEventListener("load", async_load, false);
-    var i = 0;
-    window.onscroll = function() {
-        var urls = $("[data-expanded-url]").length;
-        for (;i < urls; i++) {
-            var expanded = $("[data-expanded-url]").eq(i).attr("data-expanded-url");
-            $("[data-expanded-url]").eq(i).attr("href", expanded);
-        }
-    };
+  };
 })();
