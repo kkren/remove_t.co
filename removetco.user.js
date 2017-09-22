606201dc19be4bcac3f6b720fa7f185b69d9217a
kkren <tangjl55@gmail.com>
2017-09-22 20:56:00 +0800

    now bypass redirect in profile

--- removetco.user.js
+++ removetco.user.js
@@ -7,37 +7,41 @@
 // @require https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js
 // @run-at document-end
 // @downloadURL https://raw.githubusercontent.com/kkren/remove_t.co/master/removetco.user.js
-// @version 0.2
+// @version 0.3
 // ==/UserScript==
-    // function async_load() {
-    //   var s = document.createElement("script");
-    //   s.type = "text/javascript";
-    //   s.async = true;
-    //   s.src = "https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js";
-    //   var x = document.getElementsByTagName("script")[0];
-    //   x.parentNode.insertBefore(s, x);
-    // }
+// function async_load() {
+//   var s = document.createElement("script");
+//   s.type = "text/javascript";
+//   s.async = true;
+//   s.src = "https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js";
+//   var x = document.getElementsByTagName("script")[0];
+//   x.parentNode.insertBefore(s, x);
+// }
 
-    function replace() {
-      var i = 0;
-      var urls = $("[data-expanded-url]").length;
-      for (; i < urls; i++) {
-        var expanded = $("[data-expanded-url]").eq(i).attr("data-expanded-url");
-        $("[data-expanded-url]").eq(i).attr("href", expanded);
-      }
+function replace() {
+  var i = 0;
+  var urls = $("[href*='t.co']").length;
+  for (; i < urls; i++) {
+    if ($("[href*='t.co']").eq(i).attr("data-expanded-url") != undefined) {
+      var expanded = $("[href*='t.co']").eq(i).attr("data-expanded-url");
+    } else {
+      var expanded = $("[href*='t.co']").eq(i).attr("title");
     }
-    //异步加载
-    // if (window.attachEvent) window.attachEvent("onload", async_load);
-    // else window.addEventListener("load", async_load, false);
+    $("[href*='t.co']").eq(i).attr("href", expanded);
+  }
+}
+//异步加载
+// if (window.attachEvent) window.attachEvent("onload", async_load);
+// else window.addEventListener("load", async_load, false);
 
-    mo = new MutationObserver(function(allmutations) {
-      //alert();
-      replace();
-    });
-    var targets = document.body;
-    mo.observe(targets, {
-      'childList': true,
-      'characterData': true,
-      'subtree': true
-    });
-    replace();
+mo = new MutationObserver(function(allmutations) {
+  //alert();
+  replace();
+});
+var targets = document.querySelector(".stream");
+mo.observe(targets, {
+  'childList': true,
+  'characterData': true,
+  'subtree': true
+});
+replace();
